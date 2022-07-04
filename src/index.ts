901f5d85e4b924219b5d12b3aa8c72d3ef381417
interc0der <intercoder@whirled.io>
2022-07-04 06:31:32 -0700

    Update: Export all types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-import Registry from "./types/Registry";
 import getOrganizationByWallet from "./utils/getOrganizationByWallet";
 import registry from "./registry.json";
+import { Organization, Tier, Registry, Initiative } from './types'
 
 export { getOrganizationByWallet };
+
+export {
+    Organization, 
+    Tier, 
+    Registry, 
+    Initiative
+}
 
 export default registry as Registry;
