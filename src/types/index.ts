36581cd78d45d6b1684db6f011bcfe4fa2f9a634
interc0der <intercoder@whirled.io>
2022-07-04 06:32:48 -0700

    Create: index to export all types

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+import Initiative from "./Initiative";
+import Organization from "./Organization";
+import Registry from "./Registry";
+import Tier from "./Tier";
+
+export {
+    Initiative,
+    Organization,
+Registry,
+Tier
+}
