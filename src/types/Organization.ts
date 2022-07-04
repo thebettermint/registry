b178e1edfa414d5b472a56283f2083442c4e8db8
interc0der <intercoder@whirled.io>
2022-07-04 06:42:15 -0700

    Update: update typo in types and fix compile error

--- src/types/Organization.ts
+++ src/types/Organization.ts
@@ -1,14 +1,14 @@
 import Initiative from "./Initiative";
 
 export default interface Organization {
-  "name":string;
-  "description":string;
-  "address":string;
-  "image":string;
-  "url":string;
-  "phone":string;
-  "mailingAddress":string;
-  "country":string;
-  "EIN":string;
-  "initatives": Array<Initiative>
+  name:string;
+  description:string;
+  address:string;
+  image:string;
+  url:string;
+  phone:string;
+  mailingAddress:string;
+  country:string;
+  EIN:string;
+  initiatives: Array<Initiative>
 }
